4937a040c28a776883915adf5a121cc1528c34eb
Alex Foster <wookie471@outlook.com>
2026-03-25 20:06:27 +0000

    setting up dp manipulation

--- app/src/services/data_service.py
+++ app/src/services/data_service.py
@@ -1,4 +1,10 @@
-import json
+import json, sqlite3, pathlib, sys
+import pandas as pd
+temp = pathlib.Path(__file__).parent.resolve().as_posix()
+sys.path.append(temp)
+from core.logger import get_logger
+
+logger = get_logger()
 
 INSTRUMENTS_FILE = "app/data/instruments.json"
 PRICES_FILE = "app/data/prices.json"
@@ -22,3 +28,28 @@ def load_prices():
 def save_prices(prices):
     with open(PRICES_FILE, "w") as f:
         json.dump({"prices": prices}, f,  default=str)
+
+
+def load_instruments_db():
+    # try:
+    #     conn = sqlite3.connect("app/data/equities.db")
+    #     cur = conn.cursor()
+    #     cmd = '''SELECT * FROM '''
+
+    df = pd.read_json("app/data/instruments.json")
+    df = pd.json_normalize(df["instruments"])
+    try:
+        conn = sqlite3.connect("app/data/equities.db")
+        df.to_sql("Instruments", conn, if_exists='fail', index=False)
+        logger.info(f"Data successfully added to equities in table Instruments")
+        conn.close()
+    except Exception as e:
+        logger.error(f"Error loading the database: {e}")
+
+
+def main():
+    load_instruments_db()
+
+if __name__ == "__main__":
+    main()
+
